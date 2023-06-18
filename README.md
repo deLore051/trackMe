dacfa8e4a946d5d4691904794bb2da0ce844bace
deLore051 <stefan.dojcinovic051@gmail.com>
2023-06-18 17:13:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trackme_flutter_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/trackme_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trackme_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
